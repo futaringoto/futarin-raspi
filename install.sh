7f89b8966e24d89feb9f727d638244656462f1a0
cykps <cykps.ps@gmail.com>
2024-08-07 16:36:25 +0900

    feat: remove setup scripts and futarin-api service file

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-sudo apt update
-sudo apt install unzip
-cd /tmp
-curl -OL "https://github.com/futaringoto/futarin-raspi/archive/main.zip"
-unzip -d /opt main.zip
-mv /opt/futarin-raspi-main /opt/futarin
-cd /opt/futarin/
-./setup.sh
